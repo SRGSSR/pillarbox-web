56ee94712d512771fc025777e2e7d5b98df95d3e
amtins <andre.martins@rts.ch>
2023-07-12 16:56:19 +0200

    test(pillarbox): player initialization
    Adds a test case to ensure that when the `player` is created,
    the instance is a component of type `Player`.
    
    - adds a test case for `player` initialization
    - adds `types/jest` to dev dependencies to add autocompletion
    - adapts `linter` rules for Jest

--- test/pillarbox.spec.js
+++ test/pillarbox.spec.js
@@ -3,6 +3,20 @@ import { version } from '../package.json';
 import videojs from 'video.js';
 
 describe('Pillarbox', () => {
+  it('should create an instance of the component player', () => {
+    const videoEl = document.createElement('video');
+
+    videoEl.id = 'player';
+
+    document.body.appendChild(videoEl);
+
+    const pillarbox = new Pillarbox('player');
+
+
+    expect(pillarbox).toBeInstanceOf(Pillarbox.getComponent('Player'));
+    expect(pillarbox.id()).toEqual('player');
+  });
+
   describe('VERSION', () => {
     it('should contain the version provided by the package.json file', () => {
       expect(Pillarbox.VERSION.pillarbox).toEqual(version);
