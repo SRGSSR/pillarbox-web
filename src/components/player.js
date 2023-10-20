865be77e21dbd1c7719b23c38ec04653c89eedc5
amtins <andre.martins@rts.ch>
2023-10-20 17:27:06 +0200

    feat(player): simplifies text track handling
    Resolves #35 by adding the `textTrack` function to simplify
    text track retrieval and selection. This function allows to:
    
    - retrieve the currently active text track
    - activate a text track based on `language` and `kind` properties
    
    Also, when selecting a text track, if the `kind` property is not satisfied,
    the first text track satisfying the `language` condition will be returned,
    or `undefined` if no condition is satisfied. During this process,
    all text tracks in mode `showing` are `disabled`.
    
    ```javascript
    // Get the current text track
    player.textTrack();
    
    // Activate a text track
    player.textTrack({language:'en', kind:'captions'})
    
    // Disable all text tracks
    player.textTrack('off')
    ```
    
    - add `textTrack` function
    - export the registered component directly
    - add test coverage

--- src/components/player.js
+++ src/components/player.js
@@ -41,9 +41,65 @@ class Player extends videojs.getComponent('player') {
 
     return audioTrack;
   }
+
+  /**
+   * A getter/setter for the media's text track.
+   * Activates the text track according to the language and kind properties.
+   * Falls back on the first text track found if the kind property is not satisfied.
+   * Disables all subtitle tracks that are `showing` if the `trackSelector` is truthy but does not satisfy any condition.
+   *
+   * @param {TrackSelector} [trackSelector]
+   *
+   * @example
+   * // Get the current text track
+   * player.textTrack();
+   *
+   * @example
+   * // Disable all text tracks has a side effect
+   * player.textTrack('off');
+   * player.textTrack({});
+   *
+   * @example
+   * // Activate an text track based on language and kind properties
+   * player.textTrack({language:'en', kind:'captions'});
+   *
+   * @example
+   * // Activate first text track found corresponding to language
+   * player.textTrack({language:'fr'});
+   *
+   * @return { import('video.js/dist/types/tracks/audio-track').default|undefined }
+   */
+  textTrack(trackSelector) {
+    const textTracks = Array.from(this.player().textTracks()).filter(
+      (textTrack) => !['chapters', 'metadata'].includes(textTrack.kind)
+    );
+
+    if (!trackSelector) {
+      return textTracks.find((textTrack) => textTrack.mode === 'showing');
+    }
+
+    textTracks.forEach((textTrack) => (textTrack.mode = 'disabled'));
+
+    const { kind, language } = trackSelector;
+    const textTrack =
+      textTracks.find((textTrack) => {
+        if (textTrack.language === language && textTrack.kind === kind) {
+          textTrack.mode = 'showing';
+        }
+
+        return textTrack.mode === 'showing';
+      }) ||
+      textTracks.find((textTrack) => {
+        if (textTrack.language === language) {
+          textTrack.mode = 'showing';
+        }
+
+        return textTrack.mode === 'showing';
+      });
+
+    return textTrack;
+  }
 }
 
 // Overrides the default video.js player component
-videojs.registerComponent('player', Player);
-
-export default Player;
+export default videojs.registerComponent('player', Player);
