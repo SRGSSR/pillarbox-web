e6d2cad538a1f5b73ab06e809962a9069e522f64
amtins <andre.martins@rts.ch>
2023-10-20 17:27:06 +0200

    feat(player): simplifies text track handling
    Resolves #35 by adding the `textTrack` function to simplify
    text track retrieval and selection. This function allows to:
    
    - retrieve the currently active text track
    - activate a text track based on `language` and `kind` properties
    
    Also, when selecting a text track, if the `kind` property is not satisfied,
    the first text track satisfying the `language` condition will be returned,
    or `undefined` if no condition is satisfied. During this process,
    all text tracks in mode `showing` are `disabled`.
    
    ```javascript
    // Get the current text track
    player.textTrack();
    
    // Activate a text track
    player.textTrack({language:'en', kind:'captions'})
    
    // Disable all text tracks
    player.textTrack('off')
    ```
    
    - add `textTrack` function
    - export the registered component directly
    - add test coverage

--- test/components/player.spec.js
+++ test/components/player.spec.js
@@ -61,4 +61,63 @@ describe('Player', () => {
       expect(player.audioTrack({ language: 'fr' })).toBeUndefined();
     });
   });
+
+  describe('textTrack', () => {
+    const player = new Player(videoEl);
+
+    player.textTracks = jest.fn(() => ({
+      0: {
+        mode: 'showing',
+        id: '1',
+        kind: 'subtitles',
+        label: 'English',
+        language: 'en',
+      },
+      1: {
+        mode: 'disabled',
+        id: '2',
+        kind: 'descriptions',
+        label: 'English AD',
+        language: 'en',
+      },
+      length: 2,
+    }));
+
+    it('should return the currently active text track if the function parameter is undefined', () => {
+      expect(player.textTrack()).toMatchObject({
+        mode: 'showing',
+        id: '1',
+        kind: 'subtitles',
+        label: 'English',
+        language: 'en',
+      });
+    });
+    it('should enable and return the text track according to the language and kind properties', () => {
+      expect(
+        player.textTrack({ language: 'en', kind: 'descriptions' })
+      ).toMatchObject({
+        mode: 'showing',
+        id: '2',
+        kind: 'descriptions',
+        label: 'English AD',
+        language: 'en',
+      });
+    });
+    it('should enable and return the text track according to the language property if the kind property does not satisfy the condition', () => {
+      expect(
+        player.textTrack({ language: 'en', kind: 'invalid' })
+      ).toMatchObject({
+        mode: 'showing',
+        id: '1',
+        kind: 'subtitles',
+        label: 'English',
+        language: 'en',
+      });
+    });
+    it('should return undefined and disable every active text track if the language and kind properties do not satisfy the condition', () => {
+      expect(player.textTrack({ language: 'fr' })).toBeUndefined();
+      expect(player.textTracks.mock.results[0].value[0].mode).toBe('disabled');
+      expect(player.textTracks.mock.results[0].value[1].mode).toBe('disabled');
+    });
+  });
 });
