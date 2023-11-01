93a28a64c4e5133db35900ca4abf359de19b267b
amtins <andre.martins@rts.ch>
2023-11-01 19:00:41 +0100

    chore(npm): generate npm pack friendly compiled files
    Resolves #42 and allows to compile the project in order be able to generate
    a tarball to be used as an npm dependency. Compilation will produce the following files :
    - `pillarbox.css`, at this stage contains only the default styles for video.js
    - `pillarbox.js`, offers the SRG SSR experience
    - `pillarbox.core.js`, contains only video.js enhancements
    
    Tarball generation is not part of this PR as it is already configured to be
    generated by semantic-release.
    
    - update `package.json` file
       - add `files` that will be included in the package
       - add parcel compilation `targets`
       - add build `scripts` for js and css
    - add `terserrc` to remove comments from builds
    - add `rimraf` dev dependency
    - add `build.js` file which will contain the various dependencies to be exposed

---
+++ build.js
@@ -0,0 +1,5 @@
+import SrgSsr from './src/middleware/srgssr.js';
+import Player from './src/components/player.js';
+import Pillarbox from './src/pillarbox.js';
+
+export { Pillarbox as default, Player, SrgSsr };
