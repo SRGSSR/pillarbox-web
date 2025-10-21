49d7416d1e507603e30c741c8a941f0066315016
amtins <amtins.dev@gmail.com>
2025-10-21 20:04:23 +0200

    feat(srg-analytics): migrate to event variables

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  base: './'
+  base: './',
+  build : {
+    rollupOptions : {
+      input : {
+        index: resolve(__dirname, 'index.html'),
+        multi: resolve(__dirname, 'multi.html')
+      }
+    }
+  }
 });
