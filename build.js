974f2b6f2b6b865debaebee4cf08920eff9ed621
amtins <amtins.dev@gmail.com>
2026-05-19 18:34:06 +0200

    feat(image): export utility class
    Export the Image class to allow reuse in other projects that can benefit from
    it, such as the Google Cast Receiver.
    
    - rename the export so it's more descriptive

--- build.js
+++ build.js
@@ -5,10 +5,12 @@ import DataProvider from './src/dataProvider/services/DataProvider.js';
 import MediaComposition from './src/dataProvider/model/MediaComposition.js';
 import PillarboxMonitoring from './src/trackers/PillarboxMonitoring.js';
 import SRGAnalytics from './src/trackers/SRGAnalytics.js';
+import { default as ImageUtils } from './src/utils/Image.js';
 
 export {
   Pillarbox as default,
   DataProvider,
+  ImageUtils,
   MediaComposition,
   PillarboxMonitoring,
   Player,
